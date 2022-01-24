c28a182458ea3b0e701756eb0b1ced8c98071914
MattRuetz <matt.b.ruetz@gmail.com>
2022-01-24 13:00:36 -0500

    remove bcrypt functionality, replace with Passport, passport-local-mongoose and express-session. Add sessions & cookies for UX which work with local mongo(ose) DB. Keep comments of past encryption plugins for posterity.

--- app.js
+++ app.js
@@ -4,11 +4,18 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
+const session = require("express-session");
+const passport = require("passport");
+const passportLocalMongoose = require("passport-local-mongoose");
+
 const app = express();
+
 //const encrypt = require("mongoose-encryption");
+
 //const md5 = require("md5");
-const bcrypt = require("bcrypt");
-const saltRounds = 10;
+
+// const bcrypt = require("bcrypt");
+// const saltRounds = 10; // for bcrypt
 
 
 app.use(express.static("public"));
@@ -16,6 +23,18 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({
     extended: true
 }));
+
+// setup a session
+app.use(session({
+    secret: "this is a secret",
+    resave: false,
+    saveUninitialized: false
+}));
+
+// allow passport to be used for managing sessions
+app.use(passport.initialize());
+app.use(passport.session());
+
 
 mongoose.connect("mongodb://localhost:27017/userDB");
 
@@ -35,7 +54,14 @@ const userSchema = new mongoose.Schema({
 // });
 //
 
+// use password for salt, hash, and database storage & retrieval
+userSchema.plugin(passportLocalMongoose);
+
 const User = new mongoose.model("user", userSchema);
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
 
 app.get("/", function (req, res) {
     res.render("home");
@@ -49,29 +75,54 @@ app.get("/register", function (req, res) {
     res.render("register");
 });
 
+app.get("/secrets", function (req, res) {
+    if (req.isAuthenticated()) {
+        res.render("secrets");
+    } else {
+        res.redirect("/login");
+    }
+});
+
+app.get("/logout", function (req, res) {
+    req.logout();
+    res.redirect("/");
+});
+
 app.post("/register", function (req, res) {
 
-    bcrypt.hash(req.body.password, saltRounds, function (err, hash) {
-        const newUser = new User({
-            email: req.body.username,
-            password: hash //Store hashed & salted
-        });
+    User.register({
+        username: req.body.username
+    }, req.body.password, function (err, user) {
+        if (err) {
+            console.log(err);
+            res.redirect("/register");
+        } else {
+            passport.authenticate("local")(req, res, function () {
+                res.redirect("/secrets");
+            })
+        }
+    })
 
-        newUser.save(function (err) {
-            if (err) {
-                console.log(err);
-            } else {
-                // IF user successfully registers, show secret page
-                res.render("secrets");
-            }
-        });
-    });
 });
 
 app.post("/login", function (req, res) {
-    const username = req.body.username;
-    const password = req.body.password;
 
+    const user = new User({
+        username : req.body.username,
+        password : req.body.password
+    })
+
+    req.login(user, function(err) {
+        if (err) {
+            console.log(err);
+        }
+        else {
+            //Sends a cookie to browser to authenticate, and keep user session open
+            passport.authenticate("local")(req, res, function(){
+                res.redirect("/secrets");
+            })
+        }
+    })
 
     User.findOne({
         email: username
@@ -80,11 +131,9 @@ app.post("/login", function (req, res) {
             console.log(err);
         } else {
             if (foundUser.password) {
-                bcrypt.compare(password, foundUser.password, function (err, result) {
-                    if (result === true) {
-                        res.render("secrets");
-                    }
-                });
+                if (result === true) {
+                    res.render("secrets");
+                }
             }
         }
     })
